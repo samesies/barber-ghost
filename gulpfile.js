ea13a799566b9068f7902c556f828780d6de7f5e
thomasvaeth <thomas.vaeth@gmail.com>
2017-10-21 20:18:02 -0700

    Added task to zip theme.

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ const sass = require('gulp-sass');
 const source = require('vinyl-source-stream');
 const stylelint = require('gulp-stylelint');
 const uglify = require('gulp-uglify');
+const zip = require('gulp-zip');
 
 gulp.task('stylelint', () => {
   return gulp.src([
@@ -66,6 +67,17 @@ gulp.task('browserify', () => {
   .pipe(uglify())
   .pipe(rename({suffix: '.min'}))
   .pipe(gulp.dest('./assets/js'));
+});
+
+gulp.task('distribute', () => {
+  return gulp.src([
+    './*',
+    '!./.DS_Store',
+    '!./.git',
+    '!./node_modules'
+  ])
+  .pipe(zip('barber-ghost.zip'))
+  .pipe(gulp.dest('../'))
 });
 
 gulp.task('build', ['sass', 'amp', 'browserify']);
